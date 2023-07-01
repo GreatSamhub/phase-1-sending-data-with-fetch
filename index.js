d3533a567d593534cc743f68751abbf26e79346f
+Sam Thongo <+samuelthongo73@gmail.com>
2023-07-01 13:50:17 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(userName = 'Sam', userEmail = 'sam12@gmail.com'){
+    const url = 'http://localhost:3000/users'
+    const data = {
+        "name": userName,
+        "email": userEmail
+    }
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(data => {
+        const id = data.id
+        const userElement = document.createElement("div")
+        userElement.textContent = id
+        document.body.appendChild(userElement);
+    })
+    .catch(error => {
+        const errorElement = document.createElement("div");
+        errorElement.textContent = error.message;
+        document.body.appendChild(errorElement);
+    })
+}
+document.addEventListener('DOMContentLoaded', submitData)
